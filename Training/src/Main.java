6f470f2e8c0ce9eb344f457d2e57669842c55c16
Shane Sweeney <shane24sweeney@gmail.com>
2020-08-15 13:53:24 -0400

    Training just like running
    Going over and refreshing knowledge

---
+++ Training/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void Main(String[] args) {
+
+    }
+}
